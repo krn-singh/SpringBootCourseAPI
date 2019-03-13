df12ea9c1e4517ed97d54e1a4adf6da41b9243e6
krn-singh <karan_kalewal@yahoo.co.in>
2019-03-13 14:09:55 -0400

    Renaming the packages

---
+++ course-api-mysql/src/main/java/com/upgrade/springbootstarter/dao/CourseRepository.java
@@ -0,0 +1,17 @@
+package com.upgrade.springbootstarter.dao;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.upgrade.springbootstarter.model.Course;
+
+//This will be AUTO IMPLEMENTED by Spring into a Bean called TopicRepository
+//CRUD refers Create, Read, Update, Delete
+
+@Repository
+public interface CourseRepository extends CrudRepository<Course, Integer> {
+
+	public List<Course> findByTopicId(int topicId);
+}
---
+++ course-api-mysql/src/main/java/com/upgrade/springbootstarter/dao/TopicRepository.java
@@ -0,0 +1,13 @@
+package com.upgrade.springbootstarter.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.upgrade.springbootstarter.model.Topic;
+
+//This will be AUTO IMPLEMENTED by Spring into a Bean called TopicRepository
+//CRUD refers Create, Read, Update, Delete
+@Repository
+public interface TopicRepository extends CrudRepository<Topic, Integer> {
+
+}
